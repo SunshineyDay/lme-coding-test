778088fcc7ffc03669683c02374a975e6e35a83a
Rob CW <rob@uuthree.com>
2021-01-10 14:32:03 +0000

    Check local storage for commands

--- src/state/initialState.tsx
+++ src/state/initialState.tsx
@@ -1,22 +1,34 @@
 import { IApplicationState } from './IApplicationState';
 
+export const CommandsLocalStorageKey = 'lme.codingtest.commands';
+
 export const getInitialState = (): IApplicationState => {
-	const state: IApplicationState = {
-		commands: [
-			{
-				command: 'Left',
-				rotate: -90,
-			},
-			{
-				command: 'Right',
-				rotate: 90,
-			},
-			{
-				command: 'Forward',
-				move: 1,
-			},
-		],
-	};
+	// Pick up the app's initial state - check local storage first, and if
+	// not found there then return the default command set
+	let state: IApplicationState = { commands: [] };
+
+	const storedCommands = localStorage.getItem(CommandsLocalStorageKey);
+
+	if (storedCommands) {
+		state.commands = JSON.parse(storedCommands);
+	}
+
+	state.commands = [
+		{
+			command: 'Left',
+			rotate: -90,
+		},
+		{
+			command: 'Right',
+			rotate: 90,
+		},
+		{
+			command: 'Forward',
+			move: 1,
+		},
+	];
+
+	localStorage.setItem(CommandsLocalStorageKey, JSON.stringify(state.commands));
 
 	return state;
 };
