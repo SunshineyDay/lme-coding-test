06aa50986d624201cc542e86d8b76a6976e792f4
Rob CW <rob@uuthree.com>
2021-01-13 15:10:18 +0000

    Turn on grid column sorting

--- src/components/CommandTypeGrid.tsx
+++ src/components/CommandTypeGrid.tsx
@@ -11,6 +11,7 @@ export const CommandTypeGrid: React.FC<ICommandTypeGridProps> = (props) => {
 		filter: true,
 		menuTabs: ['filterMenuTab'],
 		resizable: true,
+		sortable: true,
 	};
 
 	const frameworkComponents = {
