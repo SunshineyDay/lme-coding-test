a929493af4894fd8b850f2881efd6d26c8542512
SunshineyDay <35965287+SunshineyDay@users.noreply.github.com>
2021-01-13 11:59:11 +0000

    Delete src/state directory
    Fixing push issue

--- src/state/Action.ts
+++
@@ -1,6 +0,0 @@
-export enum Action {
-	AddCommand = 'ADD_COMMAND',
-	UpdateCommand = 'UPDATE_COMMAND',
-	DeleteCommand = 'DELETE_COMMAND',
-	LoadFile = 'LOAD_FILE',
-}
--- src/state/IReducerAction.ts
+++
@@ -1,6 +0,0 @@
-import { Action } from './Action';
-
-export interface IReducerAction {
-	type: Action;
-	payload: any;
-}
